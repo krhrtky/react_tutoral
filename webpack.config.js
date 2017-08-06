de1eab792335faaadb5746ca4ba1e90c430bc921
t-kurihara <takuya.kurihara00@gmail.com>
2017-08-06 13:51:58 +0900

    [add] index

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  context: __dirname + '/front/src',
+  context: __dirname + '/web/src',
   entry: {
     index: './index.jsx'
   },
   output: {
-    path: path.join(__dirname, '/front/bundle'),
+    path: path.join(__dirname, '/web/bundle'),
     filename: '[name].js'
   },
   module: {
@@ -19,12 +19,12 @@ module.exports = {
   resolve: {
     modules: [
       'node_modules',
-      path.resolve('/front/src')
+      path.resolve('/web/src')
     ],
     extensions: ['.js', '.jsx']
   },
   devServer: {
-    contentBase: 'front/bundle',
+    contentBase: 'web/bundle',
     inline: true,
     historyApiFallback: true,
     headers: {
